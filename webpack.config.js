592fe6469c5d5279fe9416423da98587f03bc707
KoNyaShKa <ggiziawpgg@gmail.com>
2025-05-20 19:58:58 +0300

    ddd

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');//Так как пока что пишем в среде NODE-JS, то используем require
-//f
+const webpack = require('webpack');
 module.exports = {
     mode: "development",//Пока что этап разработки
     entry: path.resolve(__dirname, 'src', 'index.js'),//Связываем пути, лучше так, чем писать полные пути
     output: {
         //Определенный шаблон для динамических названий
-        filename: "[name].[contenthash]js",//contenthash нужен для создания новой версии сайта после изменений, обновления сразу будут добавляться
+        filename: "[name].[contenthash].js",//contenthash нужен для создания новой версии сайта после изменений, обновления сразу будут добавляться
         path: path.resolve(__dirname, 'build'),
         clean: true,//Удаляем старые версии сайта
     },
-    plugins: []
+    plugins: [
+        new HTMLWebpackPlugin(),//7.29
+    ]
 }
